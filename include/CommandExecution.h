7e2e32a98e963eea4cb7454b4cb72e0139fbb9a5
Un Sung Yoo <yus1108@hotmail.com>
2025-11-04 09:58:26 +0900

    C언어에서도 호환되도록 헤더 개선

--- include/CommandExecution.h
+++ include/CommandExecution.h
@@ -2,15 +2,25 @@
 
 #define COMMAND_EXECUTION_VERSION_MAJOR 4
 #define COMMAND_EXECUTION_VERSION_MINOR 0
-#define COMMAND_EXECUTION_VERSION_PATCH 2
+#define COMMAND_EXECUTION_VERSION_PATCH 3
 
+#if defined(__cplusplus)
 #if defined(EXPORT_COMMAND_EXECUTION_DLL)
-#define COMMAND_EXECUTION_API __declspec(dllexport)
+#define COMMAND_EXECUTION_API extern "C" __declspec(dllexport)
 #elif defined(EXPORT_COMMAND_EXECUTION_DLL)
-#define COMMAND_EXECUTION_API __declspec(dllimport)
+#define COMMAND_EXECUTION_API extern "C" __declspec(dllimport)
 #else
-#define COMMAND_EXECUTION_API
+#define COMMAND_EXECUTION_API extern "C" 
 #endif
+#else
+#if defined(EXPORT_COMMAND_EXECUTION_DLL)
+#define COMMAND_EXECUTION_API extern __declspec(dllexport)
+#elif defined(EXPORT_COMMAND_EXECUTION_DLL)
+#define COMMAND_EXECUTION_API extern __declspec(dllimport)
+#else
+#define COMMAND_EXECUTION_API extern 
+#endif
+#endif // __cplusplus
 
 typedef struct
 {
@@ -48,9 +58,10 @@ enum CEDefaultCommandOption
 
 // _DefaultTarget WILL BE CALLED FIRST PRIOR TO _Options
 // THE ORDER MATTERS IN _Options (THIS FUNCTION WILL CALL CommandOption::FuncPtr IN ORDER OF DECLARATION in _Options)
-extern "C" COMMAND_EXECUTION_API void CE_HandleCommandArgsA(const int _Argc, const char* const _Argv[], CEKeylessOptionA _DefaultTarget, CECommandOptionA* _Options, const size_t _OptionCount);
-extern "C" COMMAND_EXECUTION_API void CE_HandleCommandArgsW(const int _Argc, const wchar_t* const _Argv[], CEKeylessOptionW _DefaultTarget, CECommandOptionW* _Options, const size_t _OptionCount);
+COMMAND_EXECUTION_API void CE_HandleCommandArgsA(const int _Argc, const char* const _Argv[], CEKeylessOptionA _DefaultTarget, CECommandOptionA* _Options, const size_t _OptionCount);
+COMMAND_EXECUTION_API void CE_HandleCommandArgsW(const int _Argc, const wchar_t* const _Argv[], CEKeylessOptionW _DefaultTarget, CECommandOptionW* _Options, const size_t _OptionCount);
 #if defined(_WIN32)
-extern "C" COMMAND_EXECUTION_API void CE_HandleWinAPICmdLine(const wchar_t* _CommandLine, CEKeylessOptionW _DefaultTarget, CECommandOptionW* _Options, const size_t _OptionCount);
+COMMAND_EXECUTION_API void CE_HandleWinAPICmdLine(const wchar_t* _CommandLine, CEKeylessOptionW _DefaultTarget, CECommandOptionW* _Options, const size_t _OptionCount);
 #endif
+
 
